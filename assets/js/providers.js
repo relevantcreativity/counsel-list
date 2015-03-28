bb434ce0f23bada052ed3eeb556759ad588e6e92
Jeremy Hamel <me@jeremyjhamel.com>
2015-03-28 13:20:55 -0400

    remove output

--- assets/js/providers.js
+++ assets/js/providers.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('input[name=toggleCentralIssue]').change(function(){
         var v = $(this).val();
-        console.log(v);
         $('div[data-central-child] select').val('').trigger("chosen:updated");
         $('div[data-central-child]').hide();
         $('div[data-central-child='+ v +']').show();
